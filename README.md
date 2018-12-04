bf59b41e4924f628147e0ebbf672e4e5cc6035a4
Kara Nichols <lnicholskara@gmail.com>
2018-12-03 21:31:29 -0800

    basic html and css

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Word-Guess-Game
+
+This is a hangman style game designed around the theme of Harry Potter!
+
+Users will type any letter to get started, and then have 10 guesses to figure out themed word.
+
+The game will track the user's wins, show the missing letters and include the letters already guessed.
---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//Computer selects random word as default.
+
+//Listen for user keystroke
+
+//Store that value in a variable
+
+//Compare that value to the word - if match, show on screen - else remove guess and include under letters guessed
+
+//Continue or loop logic through all guesses
+
+//If all letters guessed win + increase wins count
+
+//Else automatically restart game

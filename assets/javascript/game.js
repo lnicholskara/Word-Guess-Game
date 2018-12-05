9a0e18b1ede01fc35614d0c9b4edebb808356067
Kara Nichols <lnicholskara@gmail.com>
2018-12-04 18:26:42 -0800

    working dashes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,11 @@ var words = [
     'magic'];
 
 var guessingWord
-var lettersGuessed
+var lettersGuessed = []
 var numOfGuesses = 10;
 var wins = 0;
-var guessingWordDisplay
+var answerArray
+var remainingLetters
 
 //Computer displays preset values, selects random word as default and shows the letters as dashes.
 
@@ -21,10 +22,34 @@ function getItem() {
     
     guessingWord = words[Math.floor(Math.random() * words.length)];
 
-    guessingWordDisplay.textContent = []
-        for (var i = 0; i < guessingWord.length; i ++)
-        guessingWordDisplay.push('-');
+    answerArray = [];
+    for (var i = 0; i < guessingWord.length; i++) {
+        answerArray[i] = "_";}
 
+    answerArrayDisplay.textContent = answerArray.join(" ")
+
+    remainingLetters = guessingWord.length;
+    
+}
+
+while (remainingLetters > 0) {
+    document.onkeyup = function(event) 
+      {var userChoice = event.key;
+      console.log(userChoice);
+
+
+
+      if (userChoice < 0) {
+        lettersGuessed[i] = event.key;
+        lettersGuessedDisplay.textContent = lettersGuessed
+
+        }
+      
+      }
+    // Game code goes here
+    // Show the player their progress
+    // Take input from the player
+    // Update answerArray and remainingLetters for every correct guess
 }
 
 //Listen for user keystroke.
@@ -37,4 +62,4 @@ function getItem() {
 
 //If all letters guessed win + increase wins count
 
-//Else automatically restart game
+//Else automatically restart 

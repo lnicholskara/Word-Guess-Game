aa84482b9012d0c71bf9e1fa5cc657de56cbe25d
Kara Nichols <lnicholskara@gmail.com>
2018-12-07 22:33:58 -0800

    basic working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,88 +6,117 @@ var words = [
     'voldemort',
     'magic'];
 
-var guessingWord
-var lettersGuessed = []
+var guessingWord;
+let lettersGuessed = [];
 var numOfGuesses = 10;
 var wins = 0;
-var answerArray
+var answerArray;
 
 //Computer displays preset values, selects random word as default and shows the letters as dashes.
+//This function runs on load, when game is won or lost.
 
 function getItem() {
 
-    winsDisplay.textContent = wins;
+    numOfGuesses = 10;
     numOfGuessesDisplay.textContent = numOfGuesses;
     
     guessingWord = words[Math.floor(Math.random() * words.length)];
 
     answerArray = [];
-
+    
     for (var i = 0; i < guessingWord.length; i++) {
         answerArray[i] = "_";
     }
 
     answerArrayDisplay.textContent = answerArray.join(" ");
-
+    lettersGuessed.length = 0;
     lettersGuessedDisplay.textContent = lettersGuessed;
-    
+
+}
+
+function setScore() {
+    winsDisplay.textContent = wins;
 }
 
 // Take input from the player or listen for user keystroke.
 document.onkeyup = function(event) {
-    var userChoice = event.key;
-    
+    var userChoice = event.key;    
+        
     // Check if the letter the user chose matches any of the guessingWord's letters
-    
+    var correctGuess = false;
+    var answerValue = guessingWord.length;
+ 
 
     for (var i = 0; i < guessingWord.length; i++) {
+        
         var letter = guessingWord[i];
 
-        console.log(letter);
-        console.log(userChoice);
+        //If user chose correct letter, put input into answerArray and print to document.
 
         if (userChoice.toLowerCase() == letter) {
             answerArray[i] = letter;
-            console.log("true-1");
+            answerArrayDisplay.textContent = answerArray.join(" ");
+            correctGuess = true;
+            answerValue--;
+            console.log(answerValue);
+        } 
+
+        else {
+            console.log("wrongGuess");
+
+        }
+
+    }
+
+    //Check if user input is original guess. If not, stops duplicate entries from being displayed.
+
+    for (var j = 0; j < lettersGuessed.length; j++) {
+        var alreadyGuessed = lettersGuessed[j];
+
+
+        if (userChoice.toLowerCase() == alreadyGuessed) {
+            correctGuess = true;
             break;
         }
 
         else {
-            console.log("false-1");
+            console.log("originalGuess");
         }
-
     }
 
-    // Show the player their progress
-    // Update answerArrayDisplay for every correct guess
+    //If user input was not correct or a duplicate entry, count the letter as a guess and display.
 
-    if (userChoice.toLowerCase() == letter) {
-        answerArrayDisplay.textContent = answerArray.join(" ");
-        console.log("true-2");
-    }
-    
-    ///Need to add else if option for if user guesses the same letter more than once!!!
-
-    // Update numOfGuessesDisplay and lettersGuessedDisplay for every correct guess
-    
-    else {
+    if (correctGuess === false) {
         numOfGuesses--;
         numOfGuessesDisplay.textContent = numOfGuesses;
-
+    
         lettersGuessed.push(userChoice);
         lettersGuessedDisplay.textContent = lettersGuessed;
-        console.log("false-2");
+
+    }
+
+    else {
+        console.log("correct guess made")
+
     }
     
+
     // Check if the game is over and if so, restart the game
 
-    if (answerArray = ["h", "o", "g", "w", "a", "r", "t", "s"] || answerArray == ["m", "u", "g", "g", "l", "e"] || answerArray == ["v", "o", "l", "d", "e", "m", "o", 'r', "t"] || answerArray == ["m", "a", "g", "i", "c"]) {
+    
+    console.log(answerValue);
+    console.log (answerArray.join(''));
+    console.log(guessingWord);
+  
+
+    if (answerArray.join('') == guessingWord) {
         wins++;
+        winsDisplay.textContent = wins;
         getItem();
         console.log("true-3");
-    }
+    } 
 
-     if (numOfGuessesDisplay >= 0) {
+    else if (numOfGuesses <= 0) {
         getItem();
         console.log("false-3");
     }
